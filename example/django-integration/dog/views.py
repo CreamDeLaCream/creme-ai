5b7f1a7be04417d06fece8546f2dfa1445f50c7f
rigyeonghong <zxcvbee@naver.com>
2022-02-23 01:34:25 +0900

    fix: combine the results of dog and human

--- example/django-integration/dog/views.py
+++ example/django-integration/dog/views.py
@@ -12,12 +12,12 @@ def index(request):
     landmark_detector_path = "landmark detector path"
     dog_head_detector_path = "dog head detector path"
     model_path = "model path"
-    human_image_path = "humanemotion/image.png"
+    dog_result = dog_model.DogModel(image_path, landmark_detector_path, dog_head_detector_path, model_path).predict()
 
-    result = dog_model.DogModel(
-        image_path, landmark_detector_path, dog_head_detector_path, model_path).predict()
-    m = human_model.RMN()
+    human_image_path = "humanemotion/image.png"
     img = cv2.imread(human_image_path)
-    human_result = m.detect_emotion_for_single_frame(
-        img)
-    return HttpResponse(result, human_result)
+    human_result = human_model.RMN().detect_emotion_for_single_frame(img)
+
+    result = dog_result ,human_result
+
+    return HttpResponse(result)
