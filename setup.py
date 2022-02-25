acb024ce96d6947dd435ffaccb520974b8079369
Yeonggwang <immutable000@gmail.com>
2022-02-25 23:18:12 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='cream_ai',
+    version='0.0.1',
+    description='cream ai package',
+    url='https://github.com/CreamDeLaCream/creme-ai.git',
+    author='cream',
+    author_email='immutable000@gmail.com',
+    license='MIT',
+    packages=['dogeemotion','humanemotion'],
+    zip_safe=False,
+    install_requires=[
+        'keras==2.8.0',
+        'tensorflow==2.8.0',
+        'dlib==19.9.0',
+    ]
+) 
