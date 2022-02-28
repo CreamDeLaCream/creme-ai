3a50ba6b3b40f2933c4016006aa75e4300d16bfa
김지하 <kimjiha1112@gmail.com>
2022-02-28 21:22:00 +0900

    [fix] model path

--- example/django-integration/dog/views.py
+++ example/django-integration/dog/views.py
@@ -8,16 +8,18 @@ import cv2
 
 
 def index(request):
-    image_path = "sample image path"
+    image_path = "creme-ai\humanemotion\dog1.jpg"
+
     landmark_detector_path = "landmark detector path"
     dog_head_detector_path = "dog head detector path"
     model_path = "model path"
-    dog_result = dog_model.DogModel(image_path, landmark_detector_path, dog_head_detector_path, model_path).predict()
 
     human_image_path = "humanemotion/image.png"
     img = cv2.imread(human_image_path)
     human_result = human_model.RMN().detect_emotion_for_single_frame(img)
+    dog_result = dog_model.DogModel(
+        image_path, landmark_detector_path, dog_head_detector_path, model_path).predict()
 
-    result = dog_result ,human_result
+    result = human_result
 
-    return HttpResponse(result)
+    return HttpResponse(dog_result, result)
